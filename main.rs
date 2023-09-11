34342a9d4b47330fe0438231e983b91d74527d40
mariano mendez <marianomendez@fi.uba.ar>
2023-09-10 23:35:23 -0300

    1- se cambiaron los tipos base de la estructura de u32 a u64 2- se agrego la implementacion de potencia de un elemento finito. 3- se implemento la division de los elementos de un campo finito a partir de aplicar el fremat little teorem. TODO: atajar en todas las peraciones que ambos elementos pertenezcan al mismo campo, dhequear el primo.

--- main.rs
+++ main.rs
@@ -1,20 +1,28 @@
 use std::ops::{Add,Sub,Mul,Div};
 use std::cmp::Ordering;
 
-#[derive(Debug)]
+#[derive(Debug,Copy,Clone)]
 
 struct FiniteFieldElement{
-    numero:u32,
-    primo: u32,
+    numero:u64,
+    primo: u64,
 }
 
 impl FiniteFieldElement{
 
-    fn new (numero:u32, primo: u32) -> FiniteFieldElement{
+    fn new (numero:u64, primo: u64) -> FiniteFieldElement{
         if numero>=primo  {
             FiniteFieldElement {numero:0, primo:0}
         }else {
             FiniteFieldElement{numero:numero, primo:primo}
+        }
+    }
+
+    fn pot(self,exp:u32)->FiniteFieldElement {
+        let aux=self.numero.pow(exp)%self.primo;
+        FiniteFieldElement{
+            numero:aux,
+            primo:self.primo
         }
     }
 }
@@ -54,17 +62,26 @@ impl Mul for FiniteFieldElement{
 
 }
 
+// se realiza la division aplicando el pequenio teorema de Fermat
+// se aplica que a/b=afb^-1 ==>
+// b^(p-1)= 1 --> b^-1 =b^(p-2)
+
 impl Div for FiniteFieldElement{
     type Output = FiniteFieldElement;
 
     fn div(self, otro: FiniteFieldElement) -> FiniteFieldElement {
+        // se contruye b^(p-2)
+        let exp:u32= (self.primo - 2) as u32;
+        let pow:FiniteFieldElement = otro.pot(exp);
+        let resultado = self.numero * pow.numero;
         FiniteFieldElement { 
-            numero: (self.numero + otro.numero)% self.primo , 
+            numero: resultado % self.primo  , 
             primo: self.primo 
         }
     }
 }
 
+// sobrescribir la igualddad 
 impl PartialEq for FiniteFieldElement {
     fn eq(&self, otro: &Self) -> bool {
         self.numero == otro.numero && self.primo== otro.primo
@@ -85,4 +102,16 @@ fn main(){
     let c:FiniteFieldElement=a*b;
 
     println!("elemento finito {:?}",c);
+
+    let a=FiniteFieldElement::new(7,19);
+    println!("elemento finito {:?}",a*a*a);
+
+    //division 
+    let a=FiniteFieldElement::new(2,19);
+    let b=FiniteFieldElement::new(7,19);
+    let c:FiniteFieldElement=a/b;
+
+    println!("elemento finito {:?}",c);
+
+
 }
